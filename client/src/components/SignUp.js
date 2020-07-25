c9784388369e1b6e4036c2e6d3c2586e78ecc973
Barbara Kumor <b.kumor@yahoo.com>
2020-07-25 17:33:26 +0200

    join form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
+import {Link} from 'react-router-dom';
 
 const SignUp = () => {
+    const [username,
+        setUsername] = useState('');
+    const [chatroom,
+        setChatroom] = useState('');
+
     return (
-        <div>
-            SignUp form
-        </div>
+        <Formik
+            initialValues={{
+            username: '',
+            chatroom: ''
+        }}
+            validationSchema={Yup.object({
+            username: Yup
+                .string()
+                .required('Required'),
+            chatroom: Yup
+                .string()
+                .required('Required')
+        })}
+            onSubmit={values => {
+            console.log(values);
+        }}>{formik => (
+                <div className="form-wrapper">
+                    <form onSubmit={formik.handleSubmit}>
+                        <input type="text" id="username" {...formik.getFieldProps('username')}/>
+                        {formik.touched.username && formik.errors.username ? <div>{formik.errors.username}</div> : null}
+                        <input type="text" id="chatroom" {...formik.getFieldProps('chatroom')}/>
+                        {formik.touched.chatroom && formik.errors.chatroom ? <div>{formik.errors.chatroom}</div> : null}
+                        <button type="submit" className="btn-submit">Join</button>
+                    </form>
+                </div>
+            )}
+        </Formik>
     )
 }
 
