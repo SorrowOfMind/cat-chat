79b685c8ee9795261835040da64139a1a7c25fd5
Barbara Kumor <b.kumor@yahoo.com>
2020-07-28 18:50:41 +0200

    changes to reducer

--- client/src/components/Chat/Header.js
+++ client/src/components/Chat/Header.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {UserContext} from '../../contexts/UserContext';
 import {Link} from 'react-router-dom';
 
-const Header = ({username, chatroom}) => (
+const Header = ({username, chatroom}) => {
+    const {dispatch} = useContext(UserContext);
+    const handleClosing = () => {
+        dispatch({type: 'CLEAR_DATA'})
+    }
+    return (
     <header className="chat-header">
         <h3 className="room">Room: {chatroom}</h3>
         <h5 className="name">You are chatting as: {username}</h5>
         <Link to="/">
-            <div className="close" title="Leave chat">X</div>
+            <div className="close" title="Leave chat" onClick={handleClosing}>X</div>
         </Link>
     </header>
-)
+    )
+}
 
 export default Header;
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -7,10 +7,14 @@ export const userReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'SIGN_UP':
             const {username, chatroom} = action.payload;
-            console.log('reducer', action.payload);
             return {
                 username,
                 chatroom
+            }
+        case 'CLEAR_DATA':
+            return {
+                username: '',
+                chatroom: ''
             }
         default:
             return state;
